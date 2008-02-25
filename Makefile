5a056fb59d77e9186120767c1e7a25c0252d072b
Alexandru Scvortov <scvalex@gmail.com>
2008-02-25 17:17:53 +0200

    == dissemina ==
    getip.cpp:
      * changed the headers to standard C++ headers
    
    Makefile:
      * added the rules for server.cpp
    
    NOTE:
      * the difference between ``git commit'' and ``git commit -a'' is that
      while the first only commits the newly added files, the second commit
      *all* changes

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
-getip:
+server: server.cpp
+	g++ -Wall server.cpp -o server
+
+getip: getip.cpp
 	g++ -Wall getip.cpp -o getip
 
--- getip.cpp
+++ getip.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <stdlib.h>
+#include <cstdlib>
 #include <errno.h>
 #include <netdb.h>
 #include <sys/types.h>
