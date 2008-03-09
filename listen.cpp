25f6fd8e3b8ffe856e6309c90cd3aa7dba68d583
Alexandru Scvortov <scvalex@gmail.com>
2008-03-09 19:31:26 +0200

    listen now uses poll instead of select
    CHANGES
    =======
    
    listen.cpp:
      * listen now uses poll() instead of select()
      * the next move is to switch dissemina to poll()
      * this might solve the crashes when handling over a thousand connections

--- listen.cpp
+++ listen.cpp
@@ -9,15 +9,19 @@ using namespace std;
 #include <cstdlib>
 #include <cstring>
 #include <unistd.h>
+#include <poll.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
 const short LocalPort = 6462;
+const int nfds = 128;
 
 static char buf[1024];
 static int listener;
+
+#define MAX(a, b) ((a > b) ? (a) : (b))
 
 int main(int argc, char *argv[]) {
 	if ((listener = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
@@ -50,47 +54,58 @@ int main(int argc, char *argv[]) {
 	}
 	cout << "listen: *** listening on port " << LocalPort << " ***" << endl;
 
-	fd_set master,
-		   rfds;
+	pollfd fds[nfds];
+	for (int i(0); i < nfds; ++i)
+		fds[i].fd = -1;
 
-	FD_ZERO(&master);
-	FD_SET(listener, &master);
-	int maxfd = listener;
+	fds[0].fd = listener;
+	fds[0].events = POLLRDNORM;
 	while (1) {
-		rfds = master;
-		if (select(maxfd + 1, &rfds, NULL, NULL, NULL) == -1) {
-			perror("select");
+		//cout << "listen: waiting for poll" << endl;
+		if (poll(fds, nfds, -1) == -1) {
+			perror("poll");
 			close(listener);
 			exit(1);
 		}
 
-		for (int i(0); i <= maxfd; ++i)
-			if (FD_ISSET(i, &rfds)) {
-				if (i == listener) {
-					sockaddr_in remoteaddr;
-					socklen_t addrlen = sizeof(remoteaddr);
-					int newfd;
-					if ((newfd = accept(listener, (sockaddr*)&remoteaddr, &addrlen)) == -1)
-						perror("accept");
-					else {
-						FD_SET(newfd, &master);
-						if (newfd > maxfd)
-							maxfd = newfd;
-						cout << "listen: connection from " << inet_ntoa(remoteaddr.sin_addr) << " on socket " << newfd << endl;
-					}
+		if (fds[0].revents & POLLRDNORM) {
+			sockaddr_in remoteaddr;
+			socklen_t addrlen = sizeof(remoteaddr);
+
+			int newfd;
+			if ((newfd = accept(listener, (sockaddr*)&remoteaddr, &addrlen)) == -1) {
+				perror("accept");
+				continue;
+			} 
+
+			int i;
+			for (i = 1; i < nfds; ++i)
+				if (fds[i].fd == -1) 
+					break;
+			if (i == nfds) {
+				cout << "listen: too many clients" << endl;
+				continue;
+			}
+
+			cout << "listen: new connection on " << newfd << endl;
+			fds[i].fd = newfd;
+			fds[i].events = POLLRDNORM;
+		}
+
+		for (int i(1); i < nfds; ++i)
+			if (fds[i].revents & POLLRDNORM) {
+				cout << "listen: socket " << fds[i].fd << " seems to have data to read" << endl;
+				int nbytes;
+				if ((nbytes = recv(fds[i].fd, buf, sizeof(buf), 0)) <= 0) {
+					if (nbytes == 0)
+						cout << "listen: socket " << fds[i].fd << " closed" << endl;
+					else
+						perror("recv");
+					close(fds[i].fd);
+					fds[i].fd = -1;
 				} else {
-					int nbytes;
-					if ((nbytes = recv(i, buf, sizeof(buf), 0)) <= 0) {
-						if (nbytes == 0)
-							cout << "listen: socket " << i << " closed" << endl;
-						else
-							perror("recv");
-						close(i);
-						FD_CLR(i, &master);
-					} else {
-						cout << "listen: data from " << i << ": ``" << buf << "''" << endl;
-						memset(buf, 0, sizeof(buf));
-					}
+					cout << "listen: data from " << fds[i].fd << ": ``" << buf << "''" << endl;
+					memset(buf, 0, sizeof(buf));
 				}
 			}
 	}
