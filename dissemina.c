571af0c8e9ba9aaf692fac7bd0d8d178a7cda3f7
Alexandru Scvortov <scvalex@gmail.com>
2008-03-13 17:45:57 +0200

    support for simultaneous requests
    CHANGES
    =======
    
    dissemina.c:
      * the old server could only handle one request at once; if the files
      are small, and the network fast, this isn't a serious problem, but
      when the files get big, or the route to a user drops the server stops
      sending out data
      * to remedy this, I've added support for many concurrent users
      * the downside is that the code is complex, based on coroutines
      (written in a language that doesn't support them)
      * bottom line is, code is crap; I'll fix it in the next couple of
      commits
    
    SIEGE
    =====
    
    ** SIEGE 2.66
    ** Preparing 100 concurrent users for battle.
    The server is now under siege...
    Lifting the server siege...      done.
    Transactions:                   3958 hits
    Availability:                 100.00 %
    Elapsed time:                  20.26 secs
    Data transferred:              12.22 MB
    Response time:                  0.01 secs
    Transaction rate:             195.36 trans/sec
    Throughput:                     0.60 MB/sec
    Concurrency:                    2.76
    Successful transactions:        3958
    Failed transactions:               0
    Longest transaction:            3.01
    Shortest transaction:           0.00

--- dissemina.c
+++ dissemina.c
@@ -26,19 +26,26 @@
 #define MAXREQSIZE 4096
 
 /* Maximum number of open network connections */
-#define NUM_FDS 16
+#define NUM_FDS 64
 
 /* On which port shall I listen? */
 #define LOCAL_PORT 6462
 
+#define DONE 1
+#define NOTDONE 0
+
 struct Request {
 	char text[MAXREQSIZE];
+	char uri[MAXURISIZE];
 	int len;
+	char ready;
+	FILE *fi;
 };
 
 int listener;
 struct pollfd fds[NUM_FDS];
 struct Request reqs[NUM_FDS];
+int requestsReady; /* the number of requests that are ready for handling */
 
 /* Display an error and quit */
 void quit_err(const char *s) {
@@ -126,7 +133,7 @@ int getRequestUri(char *uri, char *buf) {
 	if ((j == 1) && (uri[1] == '/'))
 		strcpy(uri, "./index.xml");
 
-	return 0;
+	return 1;
 }
 
 #define FileHandleNum 5
@@ -139,37 +146,43 @@ const char *FileHandle[FileHandleNum][4] = {
 };
 
 /* Send file fn to s */
-void sendPage(int s, const char *fn) {
-	FILE *fi;
+int sendPage(int idx) {
 	char conttype[32];
 	int fh = 0;
 	int i;
 	for (i = 1; i < FileHandleNum; ++i)
-		if (endsWith(fn, FileHandle[i][0])) {
+		if (endsWith(reqs[idx].uri, FileHandle[i][0])) {
 			fh = i;
 			break;
 		}
 	
-	fi = fopen(fn, FileHandle[fh][1]);
-	logprintf("sendPage", "%s", FileHandle[fh][2]);
-	strcpy(conttype, FileHandle[fh][3]);
+	int len;
+	if (!reqs[idx].fi) {
+		reqs[idx].fi = fopen(reqs[idx].uri, FileHandle[fh][1]);
+		logprintf("sendPage", "%s", FileHandle[fh][2]);
+		strcpy(conttype, FileHandle[fh][3]);
 
-	char header[256];
-	sprintf(header , "HTTP/1.1 200 OK\r\n"
-					 "Connection: close\r\n"
-					 "Content-Type: %s\r\n"
-					 "Server: Dissemina/0.0.0\r\n"
-					 "\r\n", conttype);
-	int len = strlen(header);
-	sendall(s, header, &len);
-
-	char buf[1025];
-	while (!feof(fi) && !ferror(fi)) {
-		len = fread(buf, 1, 1024, fi);
-		sendall(s, buf, &len);
+		char header[256];
+		sprintf(header , "HTTP/1.1 200 OK\r\n"
+						 "Connection: close\r\n"
+						 "Content-Type: %s\r\n"
+						 "Server: Dissemina/0.0.0\r\n"
+						 "\r\n", conttype);
+		len = strlen(header);
+		sendall(fds[idx].fd, header, &len);
 	}
 
-	fclose(fi);
+	char buf[1025];
+	if (!feof(reqs[idx].fi) && !ferror(reqs[idx].fi)) {
+		len = fread(buf, 1, 1024, reqs[idx].fi);
+		sendall(fds[idx].fd, buf, &len);
+	} else {
+		fclose(reqs[idx].fi);
+		return DONE;
+	}
+
+	logprintf("sendPage", "not done sending page");
+	return NOTDONE;
 }
 
 /* Returns true if file should be openable */
@@ -185,7 +198,7 @@ int canOpen(const char *fn) {
 }
 
 /* Send 400 Bad Request to s */
-void send400(int s) {
+int send400(int idx) {
 	logprintf("send400", "sending 400 Bad Request");
 
 	char text400[] = "HTTP/1.1 400 Bad Request\r\n"
@@ -195,11 +208,13 @@ void send400(int s) {
 					 "\r\n"
 					 "Bad Request\n";
 	int len = strlen(text400);
-	sendall(s, text400, &len);
+	sendall(fds[idx].fd, text400, &len);
+
+	return DONE;
 }
 
 /* Send 404 Not Found to s */
-void send404(int s) {
+int send404(int idx) {
 	logprintf("send404", "problem reading requested file");
 	logprintf("send404", "sending 404 Not Found");
 
@@ -210,7 +225,9 @@ void send404(int s) {
 					 "\r\n"
 					 "Not Found\n";
 	int len = strlen(text404);
-	sendall(s, text404, &len);
+	sendall(fds[idx].fd, text404, &len);
+
+	return DONE;
 }
 
 /* Create the listener and return it */
@@ -235,11 +252,15 @@ void setupListener() {
 	logprintf("setupListener", "*** listening on port %d ***", LOCAL_PORT);
 }
 
-#define close_and_unset_fd(i)\
-		close(fds[i].fd);\
-		logprintf("listen", "socket %d closed", fds[i].fd);\
-		fds[i].fd = -1;
+void close_and_unset_fd(int i) {
+	close(fds[i].fd);
+	logprintf("listen", "socket %d closed", fds[i].fd);
+	fds[i].fd = -1;
+	reqs[i].ready = 0;
+	--requestsReady;
+}
 
+/* check listener for new connections and write them into fds and reqs */
 void getNewConnections() {
 	if (fds[0].revents & POLLRDNORM) {
 		struct sockaddr_in remoteaddr;
@@ -266,8 +287,10 @@ void getNewConnections() {
 	}
 }
 
+/* read and write data from/to sockets */
 void processConnections() {
 	int i;
+	/* check if there's anything to read */
 	for (i = 1; i < NUM_FDS; ++i)
 		if (fds[i].revents & POLLRDNORM) {
 			int nbytes;
@@ -283,20 +306,26 @@ void processConnections() {
 				if (!endsWith(reqs[i].text, "\r\n\r\n"))
 					continue;
 				if (startsWith(reqs[i].text, "GET")) { // HTTP GET
-					char uri[MAXURISIZE];
-					if (getRequestUri(uri, reqs[i].text + 3) == -1) {
-						logprintf("dissemina", "malformed request");
-						send400(fds[i].fd);
-						close_and_unset_fd(i);
-					} else if (!canOpen(uri)) {
-						logprintf("dissemina", "can't find ``%s''", uri);
-						send404(fds[i].fd);
-						close_and_unset_fd(i);
-					} else {
-						sendPage(fds[i].fd, uri);
-						close_and_unset_fd(i);
-					}
-				}
+					reqs[i].ready = getRequestUri(reqs[i].uri, reqs[i].text + 3);
+					++requestsReady;
+				} else
+					fds[i].fd = -1; /* ignore all other requests */
+			}
+		}
+	
+	for (i = 1; i < NUM_FDS; ++i)
+		if (reqs[i].ready) {
+			if (reqs[i].ready == -1) {
+				logprintf("dissemina", "malformed request");
+				if (send400(i) == DONE)
+					close_and_unset_fd(i);
+			} else if (!canOpen(reqs[i].uri)) {
+				logprintf("dissemina", "can't find ``%s''", reqs[i].uri);
+				if (send404(i) == DONE)
+					close_and_unset_fd(i);
+			} else {
+				if (sendPage(i) == DONE)
+					close_and_unset_fd(i);
 			}
 		}
 }
@@ -311,8 +340,12 @@ int main(int argc, char *argv[]) {
 	fds[0].fd = listener;
 	fds[0].events = POLLRDNORM;
 	int timeout;
+	requestsReady = 0;
 	for (;;) {
 		timeout = -1;
+		if (requestsReady > 0)
+			timeout = 1;
+		logprintf("dissemina", "polling");
 		if (poll(fds, NUM_FDS, timeout) == -1)
 			quit_err("poll");
 		
